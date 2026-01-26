06fa6b57431b8b4d75fb71b14ead94da37956d66
Bhavyai Gupta <zbhavyai@gmail.com>
2026-01-25 17:45:08 -0700

    simplify movie iterate component

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -6,92 +6,108 @@ import Header from "../components/Header.jsx";
 import MovieCard from "../components/MovieCard.jsx";
 import PageNum from "../components/PageNum.jsx";
 import SearchBar from "../components/SearchBar.jsx";
+import { Col, Container, Row } from "react-bootstrap";
 
 const Movies = () => {
-  // api key is defined in file .env.development
   const apiKey = import.meta.env.VITE_OMDB_API_KEY;
 
-  let url = "https://www.omdbapi.com/?apikey=" + apiKey;
+  const [data, setData] = useState({
+    Response: "False",
+    Search: [],
+    totalResults: "0",
+    Error: "",
+  });
 
-  const [data, setData] = useState({});
   const [searchParams] = useSearchParams();
 
-  const searchTitle = searchParams.get("s");
-  const searchType = searchParams.get("type");
-  const searchYear = searchParams.get("y");
-  const resultpage = searchParams.get("page");
-
-  url += checkString(searchTitle) ? "&s=" + searchTitle : "";
-  url += checkType(searchType) ? "&type=" + searchType : "";
-  url += checkNumber(searchYear) ? "&y=" + searchYear : "";
-  url += checkNumber(resultpage) ? "&page=" + resultpage : "";
-
   useEffect(() => {
-    axios.get(url).then((response) => {
-      setData(response.data);
-    });
-  }, [url]);
+    const baseUrl = "https://www.omdbapi.com/";
+
+    // get search parameters from URL
+    const searchTitle = searchParams.get("s");
+    const searchType = searchParams.get("type");
+    const searchYear = searchParams.get("y");
+    const resultPage = searchParams.get("page");
+
+    // start building params object
+    const params = {};
+
+    if (searchTitle) {
+      params["s"] = searchTitle;
+    }
+
+    if (["movie", "series", "episode"].includes(searchType)) {
+      params["type"] = searchType;
+    }
+
+    if (searchYear) {
+      params["y"] = searchYear;
+    }
+
+    if (resultPage) {
+      params["page"] = resultPage;
+    }
+
+    console.debug(JSON.stringify(params));
+
+    // add API key after logging
+    params["apiKey"] = apiKey;
+
+    axios
+      .get(baseUrl, { params })
+      .then((response) => {
+        const resData = response.data;
+        console.debug("Titles fetched =", resData["Search"]?.length ?? 0);
+
+        setData({
+          Response: resData["Response"],
+          Search: resData["Search"] || [],
+          totalResults: resData["totalResults"] ?? "0",
+          Error: resData["Error"] ?? "",
+        });
+      })
+      .catch(() => {
+        setData({
+          Response: "False",
+          Search: [],
+          totalResults: "0",
+          Error: "Something went wrong.",
+        });
+      });
+  }, [apiKey, searchParams]);
 
   return (
     <React.Fragment>
       <Header />
       <SearchBar />
 
-      <PageNum totalResults={data["totalResults"]} bottomPadding={"1rem"} />
+      <PageNum totalResults={data["totalResults"]} bottomPadding="1rem" />
 
-      <div className="darker-background">
-        <div className="container custom-padding">
-          <div className="row">
-            <div className="col-12">
-              {data["Response"] !== "True" ? (
-                <div className="card mt-4">
-                  <div className="card-body bg-dark">
-                    <div className="text-white">{data["Error"]}</div>
-                  </div>
+      <Container className="custom-padding">
+        <Row>
+          <Col xs={12}>
+            {data["Response"] !== "True" ? (
+              <div className="card mt-4">
+                <div className="card-body bg-dark">
+                  <div className="text-white">{data["Error"]}</div>
                 </div>
-              ) : (
-                <div className="row pb-5">
-                  {data["Search"]?.map((dataItem) => {
-                    return <MovieCard key={dataItem["imdbID"]} {...dataItem} />;
-                  })}
-                </div>
-              )}
-            </div>
-          </div>
-        </div>
+              </div>
+            ) : (
+              <Row className="pb-4">
+                {data["Search"]?.map((dataItem) => {
+                  return <MovieCard key={dataItem["imdbID"]} {...dataItem} />;
+                })}
+              </Row>
+            )}
+          </Col>
+        </Row>
+      </Container>
 
-        <PageNum totalResults={data["totalResults"]} bottomPadding={"7rem"} />
-      </div>
+      <PageNum totalResults={data["totalResults"]} bottomPadding={"7rem"} />
 
       <Footer />
     </React.Fragment>
   );
 };
-
-function checkString(x) {
-  if (x === undefined || x === "" || x === null) {
-    return false;
-  }
-
-  return true;
-}
-
-function checkType(x) {
-  if (checkString(x)) {
-    if (x === "movie" || x === "series" || x === "episode") {
-      return true;
-    }
-  }
-
-  return false;
-}
-
-function checkNumber(x) {
-  if (checkString(x)) {
-    return true;
-  }
-
-  return false;
-}
 
 export default Movies;
