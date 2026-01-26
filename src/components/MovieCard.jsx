6eb4a5ed3920446880770028cdcf654e97ec0722
Bhavyai Gupta <zbhavyai@gmail.com>
2026-01-25 17:09:58 -0700

    update moviecard component to use react bootstrap components

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,41 +1,37 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { Button, Card, Col } from "react-bootstrap";
 
-const MovieCard = ({ Poster: poster, Title: title, Year: year, Type: type, imdbID: imdbId }) => {
+const MovieCard = ({ Poster: poster, Title: title, Year: year, imdbID: imdbId }) => {
+  const defaultPoster = "/no-poster.jpg";
+  const posterSrc = poster && poster.startsWith("http") ? poster : defaultPoster;
+
   const navigate = useNavigate();
 
-  let defaultPoster = "/no-poster.jpg";
-  let brokenPoster = "/no-poster.jpg";
-
-  // overwrite poster if 'N/A'
-  poster = String(poster).startsWith("http") ? poster : defaultPoster;
+  const handleNavigate = () => {
+    navigate(`/details/${imdbId}`);
+  };
 
   return (
-    <div className="col-md-3 col-sm-6 d-flex justify-content-center p-4">
-      <div className="card custom-card darker-background info-overlay-wrapper">
-        <img
+    <Col sm={6} md={3} className="d-flex justify-content-center p-4">
+      <Card className="custom-card darker-background info-overlay-wrapper">
+        <Card.Img
           className="card-img-top img-stretch"
-          src={poster}
-          alt="Movie poster"
+          src={posterSrc}
+          alt={title}
           onError={(e) => {
-            e.target.src = brokenPoster;
+            e.currentTarget.src = defaultPoster;
           }}
         />
-        <figcaption className="card-body info-overlay text-center overflow-hidden">
-          <h4 className="card-title text-white my-1 overflowing-text">{title}</h4>
-          <h6 className="card-text text-white text-center py-3">{year}</h6>
-          <button
-            type="button"
-            className="btn btn-sm btn-warning"
-            onClick={() => {
-              navigate(`/details/${imdbId}`);
-            }}
-          >
-            {type}
-          </button>
-        </figcaption>
-      </div>
-    </div>
+        <Card.Body className="info-overlay text-center overflow-hidden">
+          <Card.Title className="text-white my-1 overflowing-text">{title}</Card.Title>
+          <Card.Text className="text-white py-3">{year}</Card.Text>
+          <Button className="btn-warning" onClick={handleNavigate}>
+            View Details
+          </Button>
+        </Card.Body>
+      </Card>
+    </Col>
   );
 };
 
